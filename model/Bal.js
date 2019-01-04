3cb4211d84e8a073fba302dad4ed53d243d94ddf
Mayukha.K !!! <mayukha110@gmail.com>
2019-01-04 17:35:53 +0530

    ahfbhlscasdsdfasdasdaasdzdfaaascd

---
+++ model/Bal.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: 'balances',
+    fields: [
+      {
+        name: 'Address',
+        type: 'String',
+        length: 256
+      },
+      {
+        name: 'Balance',
+        type: 'String',
+        length: 255
+      },
+      {
+        name: 'currency',
+        type: 'String',
+        length: 255
+      }
+    ]
+  }
