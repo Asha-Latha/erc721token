3f98e025eba2e1c85f47a34fe934ec2a5c079e4f
Mayukha.K !!! <mayukha110@gmail.com>
2019-01-07 14:29:18 +0530

    jgfg

--- contract/own.js
+++ contract/own.js
@@ -117,7 +117,7 @@ module.exports = {
         require(balance === 0, 'Zero allowance')
         require(amount1 > balance, 'Amount is greater than allowance')
         
-        app.sdb.update("Approve",{owner: owner1},{amount: balance - amount});
+        app.sdb.update("Approve",{owner: owner1},{amount: balance - amount1});
 
        // var res=app.balances.transfer(Currency, amount, owner1,this.trs.senderID );
         //return res;
